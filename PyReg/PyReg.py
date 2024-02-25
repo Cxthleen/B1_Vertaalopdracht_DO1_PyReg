f842a375ea3810f86a0c98aecb3f84a472a63db0
Cxthleen <c.vanduuren16@gmail.com>
2024-02-25 22:37:29 +0100

    comments

--- PyReg/PyReg.py
+++ PyReg/PyReg.py
@@ -1,5 +1,5 @@
 import os
-
+# Start programma, stel de nodige variabelen in
 print ("========== PyReg ===========")
 print ("Welkom bij PyReg, het Python KassaSysteem voor en door DeveloperLand!")
 print ("Tel de kassa, en geef op hoeveel er nu in zit.")
@@ -8,9 +8,10 @@ keuze = 0
 dagTotaal = 0
 aantalBonnen = 0
 dagTotaalTerug = 0
-
+# Herhaal tot de gebruiker aangeeft dat het programma afgesloten moet worden
 while(not keuze == "9" ):
     os.system('cls')
+    # Toon het hoofdmenu
     print ("======== HOOFDMENU =========")
     print ("1. Nieuwe bon")
     print ("2. Retour")
@@ -18,12 +19,15 @@ while(not keuze == "9" ):
     print ("9. Afsluiten")
     print ("============================")
 
+    # Laat de gebruiker een keuze maken
     keuze = input("Maak uw keuze en druk op <ENTER>.")
-
+# Gebruiker wil een nieuwe bon maken, stel variabelen voor bon in en toon menu
     if (keuze == "1"):
         bestelKeuze = 0
         bonTotaal = 0
         bonString = ""
+
+        # Toon bon-menu
         while(not bestelKeuze == "9"):
             print ("========= BON MENU =========")
             print ("Bon "+ str(aantalBonnen))
@@ -36,26 +40,33 @@ while(not keuze == "9" ):
             print( "9. Afronden bon")
             print( "Z. Bon annuleren")
             print ("============================")
+            # Vraag gebruiker om keuze te maken
             bestelKeuze = input("Maak uw keuze en druk op <ENTER>.")
-
+            # Voeg één volwasseneticket toe aan de bon
             if ( bestelKeuze == "1"):
                 bonTotaal = bonTotaal + 19
                 bonString = bonString + "1x Volwassene                  € 19\r\n"
+            # Voeg één kinderkaartje toe aan de bon
             elif ( bestelKeuze == "2"):
                 bonTotaal = bonTotaal + 9
                 bonString = bonString + "1x kind (tot 12jr)             € 9\r\n"
+            # Voeg een familiepas toe aan de bon
             elif ( bestelKeuze == "3"):
                 bonTotaal = bonTotaal + 49
                 bonString = bonString + "1x Familiepas                  € 49\r\n"
+            # Voeg een Developerland-kaart toe aan de bon
             elif ( bestelKeuze == "4"):
                 bonTotaal = bonTotaal + 4.50
                 bonString = bonString + "1x Parkkaart                   € 4,50\r\n"
+            # Voeg een kinderwagen/bolderkar toe aan de bon
             elif ( bestelKeuze == "5"):
                 bonTotaal = bonTotaal + 6
                 bonString = bonString + "1x kinderwagen/bolderkarhuur   € 6\r\n"
+            # Voeg een parkeerkaart toe aan de bon
             elif ( bestelKeuze == "6"):
                 bonTotaal = bonTotaal + 9
                 bonString = bonString + "1x Parkeerdagkaart             € 9\r\n"
+            # Bon is klaar, toon totaal en reken af
             elif(bestelKeuze == "9"):
                 aantalBonnen = aantalBonnen +1
                 dagTotaal = dagTotaal + bonTotaal
@@ -64,17 +75,24 @@ while(not keuze == "9" ):
                 print( "Te betalen: " + str(bonTotaal) )
                 betaald = float(input("Betaald:    "))
                 terug = bonTotaal - betaald
+                # Toon retourbedrag
                 print ( "Terug:     " + str(terug))
                 input("Druk op <ENTER> om door te gaan.")
+            # Gebruiker wil de bon annuleren; reset bon variabelen en keer terug naar het hoofdmenu
             elif (bestelKeuze == "Z" or bestelKeuze == "z"):
                 bestelKeuze = 9
                 bonTotaal = 0
                 bonString = ""
+
+    # Verwerk retourbedrag in dagtotaal
+    # Gebruiker wil bedrag retourneren
     elif(keuze == "2"):
         print ("Uitvoeren terugbetaling")
+        # Vraag om bedrag en reden
         terugTeGeven = float(input( "Bedrag originele bon: "))
         reden = input("Reden retour: ")
         dagTotaalTerug = terugTeGeven
+# Gebruiker wil de dagtotalen zien
     elif(keuze == "3"):
         print ("======= DAG TOTALEN ========")
         print ("In kassa begin:   " + str(bedragInKassaBegin))
@@ -82,12 +100,15 @@ while(not keuze == "9" ):
         print ("Retour:           " + str(dagTotaalTerug))
         print ("In kassa:         " + str( bedragInKassaBegin + dagTotaal - dagTotaalTerug ))
         input("Druk op <ENTER> om door te gaan.")
+
+# Gebruiker wil het programma afsluiten, vraag om bedrag in kassa
 inKassa = float(input("Hoeveel zit er nu in de kassa?")) 
 while(not inKassa == (bedragInKassaBegin + dagTotaal - dagTotaalTerug )):
+    # Herhaal zolang het opgegeven bedrag in kassa niet overeenkomt met het berekende bedrag in kassa
     print("Je hebt een kassaverschil! Tel de kassa opnieuw")
     inKassa = float(input("Hoeveel zit er nu in de kassa?")) 
     
-
+# Kassa is correct afgesloten, toon dagtotalen
 os.system('cls')
 print("Kassa klopt, programma wordt afgesloten.")
 print ("======== DAGTOTALEN ========")
